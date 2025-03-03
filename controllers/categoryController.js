6bf06a00701545e3d7c7713da72a3192b6ba23e8
Newakk2804 <dan28.04@mail.ru>
2025-03-03 17:14:29 +0300

    part 20 is ready

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -28,6 +28,100 @@ const creareCategoryController = async (req, res) => {
     }
 };
 
+const getAllCategoryController = async (req, res) => {
+    try {
+        const categories = await categoryModel.find();
+        if (!categories) {
+            return res.status(404).send({
+                success: false,
+                message: "Not categories available",
+            });
+        }
+
+        res.status(200).send({
+            success: true,
+            totalCount: categories.length,
+            categories,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error in get all category API",
+            error,
+        });
+    }
+};
+
+const updateCategoryController = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, imageUrl } = req.body;
+
+        const updatedCategory = await categoryModel.findByIdAndUpdate(
+            id,
+            { title, imageUrl },
+            { new: true }
+        );
+
+        if (!updatedCategory) {
+            return res.status(500).send({
+                success: false,
+                message: "Not found category",
+            });
+        }
+
+        res.status(200).send({
+            success: true,
+            message: "Category updated successfully",
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error in update category API",
+            error,
+        });
+    }
+};
+
+const deleteCategoryController = async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!id) {
+            return res.status(500).send({
+                success: false,
+                messge: "Please provide category ID",
+            });
+        }
+
+        const category = await categoryModel.findById(id);
+        if (!category) {
+            return res.status(404).send({
+                success: false,
+                message: "Not found category",
+            });
+        }
+
+        await categoryModel.findByIdAndDelete(id);
+
+        res.status(200).send({
+            success: true,
+            message: "Category deleted successfully",
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error in delete category API",
+            error,
+        });
+    }
+};
+
 module.exports = {
     creareCategoryController,
+    getAllCategoryController,
+    updateCategoryController,
+    deleteCategoryController,
 };
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
-const { creareCategoryController } = require("../controllers/categoryController");
+const {
+    creareCategoryController,
+    getAllCategoryController,
+    updateCategoryController,
+    deleteCategoryController,
+} = require("../controllers/categoryController");
 
 const router = express.Router();
 
 router.post("/create", authMiddleware, creareCategoryController);
+router.get("/get-all", getAllCategoryController);
+router.put("/update/:id", authMiddleware, updateCategoryController);
+router.delete("/delete/:id", authMiddleware, deleteCategoryController);
 
 module.exports = router;
