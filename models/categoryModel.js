c4b4f8b6f1c9928a349134ed54bb4f1e2043948a
Newakk2804 <dan28.04@mail.ru>
2025-03-03 17:48:31 +0300

    part 21 is ready

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -8,7 +8,8 @@ const categorySchena = new mongoose.Schema(
         },
         imageUrl: {
             type: String,
-            default: "https://image.similarpng.com/file/similarpng/very-thumbnail/2021/09/Good-food-logo-design-on-transparent-background-PNG.png",
+            default:
+                "https://image.similarpng.com/file/similarpng/very-thumbnail/2021/09/Good-food-logo-design-on-transparent-background-PNG.png",
         },
     },
     {
